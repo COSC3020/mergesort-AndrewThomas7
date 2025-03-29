82b8a6b795f5dc61f4c6d92fc7eb8f6c9dc4276c
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-03-28 20:05:23 -0600

    Update code.js

--- code.js
+++ code.js
@@ -6,35 +6,60 @@ function shift(array,left,right) {
 
 }
 
-function mergesort(array){
-    var end =array.length-1
-    for(var n=1; n<array.length; n*=2){
-        for(m=0;m<array.length;m++){
-            var left=m;
-            var right=n+m;
-            var mid= Math.ceil((left+right)/2)
-            while(left <= mid && right <= end){
+
+function mergeSort(array) {
+    var end = array.length - 1
+    for (var n = 1; n < array.length; n *= 2) {
+        for (m = 0; m < array.length; m++) {
+            var left = m;
+            var right = n + m;
+            var mid = Math.ceil((left + right) / 2)
+            while (left <= mid && right <= end) {
                 if (array[left] > array[right]) {
-                    var temp=array[right];
-                    shift(array,left,right);
-                    array[left]=temp;
-                    if(m==0){
-                        left+=1;
-                        right+=1;
-                        mid+=1;
+                    var temp = array[right];
+                    shift(array, left, right);
+                    array[left] = temp;
+                    if (m == 0) {
+                        left += 1;
+                        right += 1;
+                        mid += 1;
                     }
-                        left+=m;
-                        right+=m;
-                        mid+=1;
+                    left += m;
+                    right += m;
+                    mid += 1;
                 }
-                else if(array[left]==array[right]){
-                        right+=1
+                else if (array[left] == array[right]) {
+                    right += 1
                 }
-                else{
-                    left+=1;
+                else {
+                    left += 1;
                 }
-        }
+            }
+
         }
     }
-    return array;
+        for (j = 0; j < array.length; j++) {
+            var left = 0;
+            var right = j+1;
+            var mid = Math.ceil((left + right) / 2)
+            while (left <= mid && right <= end) {
+                if (array[left] > array[right]) {
+                    var temp = array[right];
+                    shift(array, left, right);
+                    array[left] = temp;
+                    if (j == 0) {
+                        left += 1;
+                        right += 1;
+                        mid += 1;
+                    }
+                    left += j;
+                    right += j;
+                    mid += 1;
+                }
+            else{
+                left+=1;
+            }
+            }
+        }   
+        return array;
 }
