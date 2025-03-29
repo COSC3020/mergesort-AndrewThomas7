11e93ede88f5a44eddaac1480b11780231641e34
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-03-28 19:11:46 -0600

    Update code.js

--- code.js
+++ code.js
@@ -5,29 +5,30 @@ function shift(array,left,right) {
     return array;
 
 }
-function mergesort(array){
+function mergeSort(array){
     var end =array.length-1
-    if(array.length<=1){
-        return array;
-    }
     for(var n=1; n<array.length; n*=2){
-        for(var m=0;m<array.length-1;m+=2*n){
+        for(m=0;m<array.length;m++){
             var left=m;
-            var right=m+n;
+            var right=n+m;
             var mid= Math.ceil((left+right)/2)
-            while(left < right && right< m+2*n){
+            while(left <= mid && right <= end){
                 if (array[left] > array[right]) {
                     var temp=array[right];
                     shift(array,left,right);
                     array[left]=temp;
-                        left++;
-                        right++;
-                        mid++;
+                        left+=m;
+                        right+=m;
+                        mid+=1;
                 }
                 else{
                     left+=1;
                 }
-
+        }
+        }
+    }
+    return array;
+}
             }
         }
     }
