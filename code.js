818fe4630a5e1b15f9d25235b6549c1a6765d481
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-03-30 23:16:17 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,62 +1,37 @@
-function shift(array,left,right) {
-    for (var i = right; i > left; i--) {
-        array[i] = array[i - 1]
+
+function shift(array,left,right){
+    for(i=right;i>left;i--){
+        array[i]=array[i-1]
     }
     return array;
-
 }
-
-function mergesort(array) {
-    var end = array.length - 1
-    if(array.length<=1){
-        return array;
-    }
-    for (var n = 1; n < array.length; n *= 2) {
-        for (m = 0; m < array.length; m++) {
-            var left = m;
-            var right = n + m;
-            var mid = Math.ceil((left + right) / 2)
-            while (left <= mid && right <= end) {
-                if (array[left] > array[right]) {
-                    var temp = array[right];
-                    shift(array, left, right);
-                    array[left] = temp;
-                    if (m == 0) {
-                        left += 1;
-                        right += 1;
-                        mid += 1;
-                    }
-                    left += m;
-                    right += m;
-                    mid += 1;
-                }
-                else if (array[left] == array[right]) {
-                    right += 1
-                }
-                else {
-                    left += 1;
-                }
-            }
-
+// console.log([2,5,3,4],0,2)
+function merge(array,left,right,n){
+    while(left<right&&right<n){
+        if(array[left]>array[right]){
+            var temp =array[right];
+            shift(array,left,right)
+            array[left]=temp;
+            left+=1;
+            right+=1;
+        }
+        else{
+            left+=1;
         }
     }
-        for (j = 0; j < array.length; j++) {
-            var left = 0;
-            var right = j+1;
-            var mid = Math.ceil((left + right) / 2)
-            while (left <= mid && right <= end) {
-                if (array[left] > array[right]) {
-                    var temp = array[right];
-                    shift(array, left, right);
-                    array[left] = temp;
-                        left += 1;
-                        right += 1;
-                        mid += 1;
-                }
-            else{
-                left+=1;
-            }
-            }
-        }   
-        return array;
 }
+// console.log(merge(testarr,0,1))
+
+function mergesort(array){
+    for(var i=1;i<array.length;i*=2){
+        for(var j=0;j<array.length;j+=i*2){
+            var left=j;
+            var right=left+i;
+            var n=2*i+j
+                merge(array,left,right,n)
+        }
+    }
+    return array;
+}
+//testarr=[8,2,5,3,9,4,1,7]
+//console.log(mergeSort(testarr))
